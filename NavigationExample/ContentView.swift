9b83ddc31701875a662b080ec01e2d0b38ac148a
maxwellyue <756332309@qq.com>
2023-03-08 10:37:10 +0800

    Initial Commit

---
+++ NavigationExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NavigationExample
+//
+//  Created by yueyue max on 2023/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationExample/NavigationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExampleApp.swift
+//  NavigationExample
+//
+//  Created by yueyue max on 2023/3/8.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
